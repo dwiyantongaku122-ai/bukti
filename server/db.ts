afd7b946d412d74019f4d1f360d1973fb73f0084
dwiyantongaku122-ai <dwiyantongaku122@gmail.com>
2026-02-08 13:28:44 +0700

    Update db.ts

--- server/db.ts
+++ server/db.ts
@@ -1,6 +1,6 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import pg from "pg";
-import * as schema from "@shared/schema";
+import { api } from "@shared/routes";
 
 const { Pool } = pg;
 
